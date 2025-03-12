418853f8160d1bc6e9521ad729b2f194a425060a
Howard Braham <howrad@gmail.com>
2025-03-11 17:56:13 -0700

    try test-reporter

--- test/e2e/run-all.js
+++ test/e2e/run-all.js
@@ -305,6 +305,9 @@ async function main() {
     myTestList = testPaths;
   }
 
+  myTestList = [myTestList[0]];
+
+  // TODO: Undo this
   console.log('My test list:', myTestList);
 
   // spawn `run-e2e-test.js` for each test in myTestList
