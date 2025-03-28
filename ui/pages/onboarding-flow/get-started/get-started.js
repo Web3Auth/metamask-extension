430adb2f315ffca14922cec41c834565e9271544
Lionell Briones <llenoil@gmail.com>
2025-03-28 10:17:04 +0800

    polish getting started

--- ui/pages/onboarding-flow/get-started/get-started.js
+++ ui/pages/onboarding-flow/get-started/get-started.js
@@ -8,12 +8,17 @@ import {
   ButtonBase,
   ButtonVariant,
   ButtonBaseSize,
+  Text,
 } from '../../../components/component-library';
 import {
   Display,
   JustifyContent,
   AlignItems,
   BlockSize,
+  BackgroundColor,
+  TextVariant,
+  TextColor,
+  FontWeight,
 } from '../../../helpers/constants/design-system';
 import ButtonGroup from '../../../components/ui/button-group';
 
@@ -44,22 +49,37 @@ export default function GetStarted({}) {
     <div className="get-started">
       <div className="get-started__wrapper">
         {/* TODO: Use proper font */}
-        <div className="get-started__title">Welcome to Metamask</div>
+        <div className="get-started__title">
+          <Text
+            className="get-started__title-text"
+            variant={TextVariant.headingLg}
+            color={TextColor.primaryDefault}
+          >
+            Welcome to Metamask
+          </Text>
+        </div>
         <div className="get-started__mascot">{renderMascot()}</div>
         <Box
-          className="get-started__button"
+          className="get-started__button-container"
           display={Display.Flex}
           justifyContent={JustifyContent.center}
           alignItems={AlignItems.center}
         >
           {/* TODO: Check if this is the correct button component */}
           <ButtonBase
+            className="get-started__button"
             width={BlockSize.Full}
-            variant={ButtonVariant.Secondary}
+            variant={ButtonVariant.Primary}
             size={ButtonBaseSize.Lg}
             onClick={handleClick}
           >
-            Get started
+            <Text
+              variant={TextVariant.bodyMd}
+              color={TextColor.primaryDefault}
+              fontWeight={FontWeight.Medium}
+            >
+              Get started
+            </Text>
           </ButtonBase>
         </Box>
       </div>
