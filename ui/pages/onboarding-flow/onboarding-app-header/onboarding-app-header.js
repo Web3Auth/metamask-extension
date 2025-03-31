38fc74986f9f1915a7505866843208ddfad78fc7
Lionell Briones <llenoil@gmail.com>
2025-04-01 01:40:16 +0900

    - welcome terms of use and routes - change bg on welcome

--- ui/pages/onboarding-flow/onboarding-app-header/onboarding-app-header.js
+++ ui/pages/onboarding-flow/onboarding-app-header/onboarding-app-header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 import MetaFoxLogo from '../../../components/ui/metafox-logo';
 import Dropdown from '../../../components/ui/dropdown';
 import { getCurrentLocale } from '../../../ducks/locale/locale';
@@ -7,9 +8,11 @@ import { updateCurrentLocale } from '../../../store/actions';
 // TODO: Remove restricted import
 // eslint-disable-next-line import/no-restricted-paths
 import locales from '../../../../app/_locales/index.json';
+import { ONBOARDING_WELCOME_ROUTE } from '../../../helpers/constants/routes';
 
 export default function OnboardingAppHeader() {
   const dispatch = useDispatch();
+  const { pathname } = useLocation();
   const currentLocale = useSelector(getCurrentLocale);
   const localeOptions = locales.map((locale) => {
     return {
@@ -19,8 +22,17 @@ export default function OnboardingAppHeader() {
   });
 
   return (
-    <div className="onboarding-app-header">
-      <div className="onboarding-app-header__contents">
+    <div
+      className={`onboarding-app-header ${
+        pathname === ONBOARDING_WELCOME_ROUTE
+          ? 'onboarding-app-header--welcome'
+          : ''
+      }`}
+    >
+      <div
+        className="onboarding-app-header__contents"
+        data-theme={pathname === ONBOARDING_WELCOME_ROUTE ? 'light' : ''}
+      >
         <MetaFoxLogo unsetIconHeight isOnboarding />
         <Dropdown
           id="select-locale"
--- ui/pages/onboarding-flow/welcome/welcome.js
+++ ui/pages/onboarding-flow/welcome/welcome.js
@@ -1,216 +1,101 @@
 import EventEmitter from 'events';
-import React, { useState, useEffect, useContext } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import Mascot from '../../../components/ui/mascot';
+import { isFlask, isBeta } from '../../../helpers/utils/build-types';
 import {
-  Button,
-  ButtonSize,
+  Box,
+  ButtonBase,
   ButtonVariant,
-  Icon,
-  IconName,
-  IconSize,
+  ButtonBaseSize,
   Text,
 } from '../../../components/component-library';
 import {
-  TextVariant,
-  TextAlign,
-  FontWeight,
-  TextColor,
+  Display,
+  JustifyContent,
+  AlignItems,
   BlockSize,
-  IconColor,
+  TextVariant,
+  TextColor,
+  FontWeight,
 } from '../../../helpers/constants/design-system';
-import { useI18nContext } from '../../../hooks/useI18nContext';
-import { MetaMetricsContext } from '../../../contexts/metametrics';
-import {
-  MetaMetricsEventCategory,
-  MetaMetricsEventName,
-} from '../../../../shared/constants/metametrics';
-import {
-  setFirstTimeFlowType,
-  setTermsOfUseLastAgreed,
-} from '../../../store/actions';
-import {
-  ///: BEGIN:ONLY_INCLUDE_IF(build-main,build-beta,build-flask)
-  ONBOARDING_METAMETRICS,
-  ///: END:ONLY_INCLUDE_IF
-  ONBOARDING_SECURE_YOUR_WALLET_ROUTE,
-  ONBOARDING_COMPLETION_ROUTE,
-} from '../../../helpers/constants/routes';
-import { getFirstTimeFlowType, getCurrentKeyring } from '../../../selectors';
-import { FirstTimeFlowType } from '../../../../shared/constants/onboarding';
-import { isFlask, isBeta } from '../../../helpers/utils/build-types';
+import { ONBOARDING_GET_STARTED_ROUTE } from '../../../helpers/constants/routes';
+import TermsOfUsePopup from '../../../components/app/terms-of-use-popup';
 
-export default function OnboardingWelcome() {
-  const t = useI18nContext();
-  const dispatch = useDispatch();
-  const history = useHistory();
+export default function Welcome() {
   const [eventEmitter] = useState(new EventEmitter());
-  const currentKeyring = useSelector(getCurrentKeyring);
-  const firstTimeFlowType = useSelector(getFirstTimeFlowType);
-  const [newAccountCreationInProgress, setNewAccountCreationInProgress] =
-    useState(false);
+  const [showTermsOfUse, setShowTermsOfUse] = useState(false);
 
-  // Don't allow users to come back to this screen after they
-  // have already imported or created a wallet
-  useEffect(() => {
-    if (currentKeyring && !newAccountCreationInProgress) {
-      if (firstTimeFlowType === FirstTimeFlowType.import) {
-        history.replace(ONBOARDING_COMPLETION_ROUTE);
-      }
-      if (firstTimeFlowType === FirstTimeFlowType.restore) {
-        history.replace(ONBOARDING_COMPLETION_ROUTE);
-      } else {
-        history.replace(ONBOARDING_SECURE_YOUR_WALLET_ROUTE);
-      }
-    }
-  }, [
-    currentKeyring,
-    history,
-    firstTimeFlowType,
-    newAccountCreationInProgress,
-  ]);
-  const trackEvent = useContext(MetaMetricsContext);
-
-  const onCreateClick = async () => {
-    setNewAccountCreationInProgress(true);
-    dispatch(setFirstTimeFlowType(FirstTimeFlowType.create));
-    trackEvent({
-      category: MetaMetricsEventCategory.Onboarding,
-      event: MetaMetricsEventName.OnboardingWalletCreationStarted,
-      properties: {
-        account_type: 'metamask',
-      },
-    });
-    dispatch(setTermsOfUseLastAgreed(new Date().getTime()));
-
-    ///: BEGIN:ONLY_INCLUDE_IF(build-main,build-beta,build-flask)
-    history.push(ONBOARDING_METAMETRICS);
-    ///: END:ONLY_INCLUDE_IF
-  };
-
-  const onImportClick = async () => {
-    await dispatch(setFirstTimeFlowType(FirstTimeFlowType.import));
-    trackEvent({
-      category: MetaMetricsEventCategory.Onboarding,
-      event: MetaMetricsEventName.OnboardingWalletImportStarted,
-      properties: {
-        account_type: 'imported',
-      },
-    });
-    dispatch(setTermsOfUseLastAgreed(new Date().getTime()));
-
-    ///: BEGIN:ONLY_INCLUDE_IF(build-main,build-beta,build-flask)
-    history.push(ONBOARDING_METAMETRICS);
-    ///: END:ONLY_INCLUDE_IF
-  };
+  const history = useHistory();
 
   const renderMascot = () => {
     if (isFlask()) {
       return (
-        <img src="./images/logo/metamask-fox.svg" width="240" height="240" />
+        <img src="./images/logo/metamask-fox.svg" width="300" height="300" />
       );
     }
     if (isBeta()) {
       return (
-        <img src="./images/logo/metamask-fox.svg" width="240" height="240" />
+        <img src="./images/logo/metamask-fox.svg" width="300" height="300" />
       );
     }
     return (
-      <Mascot animationEventEmitter={eventEmitter} width="250" height="300" />
+      <Mascot animationEventEmitter={eventEmitter} width="332" height="332" />
     );
   };
 
+  const onAcceptTermsOfUse = () => {
+    history.push(ONBOARDING_GET_STARTED_ROUTE);
+  };
+
   return (
-    <div className="onboarding-welcome" data-testid="onboarding-welcome">
-      {/* <div className="onboarding-welcome__logo">
-        <MetaFoxLogo theme="light" />
-      </div> */}
-      <div className="onboarding-welcome__mascot">{renderMascot()}</div>
-
-      <div className="onboarding-welcome__title">
-        <Text
-          variant={TextVariant.displayMd}
-          as="h2"
-          textAlign={TextAlign.Center}
-          fontWeight={FontWeight.Bold}
+    <div className="welcome">
+      <div className="welcome__wrapper">
+        {/* TODO: Use proper font */}
+        <div className="welcome__title">
+          <Text
+            className="welcome__title-text"
+            variant={TextVariant.headingLg}
+            color={TextColor.primaryDefault}
+          >
+            Welcome to Metamask
+          </Text>
+        </div>
+        <div className="welcome__mascot">{renderMascot()}</div>
+        <Box
+          className="welcome__button-container"
+          display={Display.Flex}
+          justifyContent={JustifyContent.center}
+          alignItems={AlignItems.center}
         >
-          {t('welcomeToMetaMask')}!
-        </Text>
-      </div>
-
-      <ul className="onboarding-welcome__buttons">
-        <li>
-          <button className="onboarding-welcome__plain-button">
-            {
-              ///: BEGIN:ONLY_INCLUDE_IF(build-main,build-beta,build-flask)
-              <div className="onboarding-welcome__plain-button-content">
-                <img
-                  src="images/icons/google.svg"
-                  className="onboarding-welcome__social-icon"
-                  alt="Google icon"
-                />
-                <Text variant={TextVariant.bodyMd}>Continue with Google</Text>
-              </div>
-              ///: END:ONLY_INCLUDE_IF
-            }
-          </button>
-        </li>
-        <li>
-          <button className="onboarding-welcome__plain-button">
-            {
-              ///: BEGIN:ONLY_INCLUDE_IF(build-main,build-beta,build-flask)
-              <div className="onboarding-welcome__plain-button-content">
-                <Icon
-                  name={IconName.Apple}
-                  color={IconColor.iconDefault}
-                  size={IconSize.Lg}
-                />
-                <Text variant={TextVariant.bodyMd}>Continue with Apple</Text>
-              </div>
-              ///: END:ONLY_INCLUDE_IF
-            }
-          </button>
-        </li>
-        <li>
-          <div className="onboarding-welcome__or">
-            <Text
-              className="onboarding-welcome__or-text"
-              variant={TextVariant.bodyMd}
-              color={TextColor.textMuted}
-              as="div"
-            >
-              OR
-            </Text>
-          </div>
-        </li>
-        <li>
-          <Button
-            data-testid="onboarding-create-wallet"
+          {/* TODO: Check if this is the correct button component */}
+          <ButtonBase
+            className="welcome__button"
+            width={BlockSize.Full}
             variant={ButtonVariant.Primary}
-            width={BlockSize.Full}
-            size={ButtonSize.Lg}
-            onClick={onCreateClick}
+            size={ButtonBaseSize.Lg}
+            onClick={() => setShowTermsOfUse(true)}
           >
-            {
-              ///: BEGIN:ONLY_INCLUDE_IF(build-main,build-beta,build-flask)
-              t('onboardingCreateWallet')
-              ///: END:ONLY_INCLUDE_IF
-            }
-          </Button>
-        </li>
-        <li>
-          <Button
-            data-testid="onboarding-create-wallet"
-            variant={ButtonVariant.Secondary}
-            width={BlockSize.Full}
-            size={ButtonSize.Lg}
-            onClick={onImportClick}
-          >
-            {t('onboardingImportWallet')}
-          </Button>
-        </li>
-      </ul>
+            <Text
+              variant={TextVariant.bodyMd}
+              color={TextColor.primaryDefault}
+              fontWeight={FontWeight.Medium}
+            >
+              Get started
+            </Text>
+          </ButtonBase>
+        </Box>
+      </div>
+      <TermsOfUsePopup
+        isOpen={showTermsOfUse}
+        onClose={() => setShowTermsOfUse(false)}
+        onAccept={() => {
+          setShowTermsOfUse(false);
+          onAcceptTermsOfUse();
+        }}
+      />
     </div>
   );
 }
+
+Welcome.propTypes = {};
