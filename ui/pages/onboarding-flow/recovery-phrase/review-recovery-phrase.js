8211b6eb043fe5403f0e51086355b5d6df64e497
Lionell Briones <llenoil@gmail.com>
2025-04-10 10:48:47 +0800

    reveal and save srp

--- ui/pages/onboarding-flow/recovery-phrase/review-recovery-phrase.js
+++ ui/pages/onboarding-flow/recovery-phrase/review-recovery-phrase.js
@@ -1,22 +1,20 @@
 import React, { useState, useContext } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { useCopyToClipboard } from '../../../hooks/useCopyToClipboard';
+// import { useCopyToClipboard } from '../../../hooks/useCopyToClipboard';
 import { useI18nContext } from '../../../hooks/useI18nContext';
 import { ONBOARDING_CONFIRM_SRP_ROUTE } from '../../../helpers/constants/routes';
 import {
   Text,
-  Icon,
-  IconName,
   Box,
   Button,
-  ButtonPrimary,
   ButtonVariant,
+  ButtonLink,
+  ButtonLinkSize,
 } from '../../../components/component-library';
 import {
   TextVariant,
   JustifyContent,
-  IconColor,
   BlockSize,
   TextColor,
 } from '../../../helpers/constants/design-system';
@@ -25,15 +23,19 @@ import {
   MetaMetricsEventName,
 } from '../../../../shared/constants/metametrics';
 import { MetaMetricsContext } from '../../../contexts/metametrics';
+import SRPDetailsModal from '../../../components/app/srp-details-modal';
 import RecoveryPhraseChips from './recovery-phrase-chips';
 
 export default function RecoveryPhrase({ secretRecoveryPhrase }) {
   const history = useHistory();
   const t = useI18nContext();
   const { search } = useLocation();
-  const [copied, handleCopy] = useCopyToClipboard();
+  // TODO: Check on copy to clipboard
+  // const [copied, handleCopy] = useCopyToClipboard();
   const [phraseRevealed, setPhraseRevealed] = useState(false);
-  const [hiddenPhrase, setHiddenPhrase] = useState(false);
+  // TODO: Check on hide phrase
+  // const [hiddenPhrase, setHiddenPhrase] = useState(false);
+  const [showSrpDetailsModal, setShowSrpDetailsModal] = useState(false);
   const searchParams = new URLSearchParams(search);
   const isFromReminderParam = searchParams.get('isFromReminder')
     ? '/?isFromReminder=true'
@@ -42,29 +44,51 @@ export default function RecoveryPhrase({ secretRecoveryPhrase }) {
 
   return (
     <div className="recovery-phrase" data-testid="recovery-phrase">
+      {showSrpDetailsModal && (
+        <SRPDetailsModal onClose={() => setShowSrpDetailsModal(false)} />
+      )}
       <Box
         justifyContent={JustifyContent.flexStart}
         marginBottom={4}
         width={BlockSize.Full}
       >
         <Text variant={TextVariant.bodyMd} color={TextColor.textAlternative}>
-          Step 2 of 3
+          {t('stepOf', [2, 3])}
         </Text>
         <Text variant={TextVariant.headingLg}>
-          Save your Secret Recovery Phrase
+          {t('seedPhraseReviewTitle')}
         </Text>
       </Box>
       <Box marginBottom={6}>
         <Text variant={TextVariant.bodyMd} color={TextColor.textAlternative}>
-          This is your Secret Recovery Phrase. Write it down in the correct
-          order and keep it safe. If someone has your Secret Recovery Phrase,
-          they can access your wallet. Don’t share it with anyone, ever.
+          <Text variant={TextVariant.bodyMd} marginBottom={6}>
+            {t('seedPhraseReviewDetails', [
+              [
+                <ButtonLink
+                  key="seedPhraseReviewDetails"
+                  size={ButtonLinkSize.Inherit}
+                  onClick={() => {
+                    setShowSrpDetailsModal(true);
+                  }}
+                >
+                  {t('secretRecoveryPhrase')}
+                </ButtonLink>,
+              ],
+            ])}
+          </Text>
         </Text>
       </Box>
       <RecoveryPhraseChips
         secretRecoveryPhrase={secretRecoveryPhrase.split(' ')}
-        phraseRevealed={phraseRevealed && !hiddenPhrase}
-        hiddenPhrase={hiddenPhrase}
+        phraseRevealed={phraseRevealed}
+        // hiddenPhrase={hiddenPhrase}
+        revealPhrase={() => {
+          trackEvent({
+            category: MetaMetricsEventCategory.Onboarding,
+            event: MetaMetricsEventName.OnboardingWalletSecurityPhraseRevealed,
+          });
+          setPhraseRevealed(true);
+        }}
       />
       <Box width={BlockSize.Full}>
         <Button
@@ -72,9 +96,19 @@ export default function RecoveryPhrase({ secretRecoveryPhrase }) {
           variant={ButtonVariant.Primary}
           data-testid="recovery-phrase-reveal"
           className="recovery-phrase__footer--button"
-          onClick={() => console.log('Continue')}
+          disabled={!phraseRevealed}
+          onClick={() => {
+            trackEvent({
+              category: MetaMetricsEventCategory.Onboarding,
+              event:
+                MetaMetricsEventName.OnboardingWalletSecurityPhraseWrittenDown,
+            });
+            history.push(
+              `${ONBOARDING_CONFIRM_SRP_ROUTE}${isFromReminderParam}`,
+            );
+          }}
         >
-          Continue
+          {t('continue')}
         </Button>
       </Box>
     </div>
