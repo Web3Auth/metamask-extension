2db8af2e1b29b3e35a61a6d1107bc1779edb1558
Lionell Briones <llenoil@gmail.com>
2025-04-15 14:15:47 +0800

    put back follow twitter

--- ui/pages/onboarding-flow/onboarding-flow.js
+++ ui/pages/onboarding-flow/onboarding-flow.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { Switch, Route, useHistory, useLocation } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import Unlock from '../unlock-page';
@@ -31,14 +31,14 @@ import {
   createNewVaultAndRestore,
 } from '../../store/actions';
 import { getFirstTimeFlowTypeRouteAfterUnlock } from '../../selectors';
-// import { MetaMetricsContext } from '../../contexts/metametrics';
-// import Button from '../../components/ui/button';
+import { MetaMetricsContext } from '../../contexts/metametrics';
+import Button from '../../components/ui/button';
 import RevealSRPModal from '../../components/app/reveal-SRP-modal';
-// import { useI18nContext } from '../../hooks/useI18nContext';
-// import {
-//   MetaMetricsEventCategory,
-//   MetaMetricsEventName,
-// } from '../../../shared/constants/metametrics';
+import { useI18nContext } from '../../hooks/useI18nContext';
+import {
+  MetaMetricsEventCategory,
+  MetaMetricsEventName,
+} from '../../../shared/constants/metametrics';
 ///: BEGIN:ONLY_INCLUDE_IF(build-flask)
 import ExperimentalArea from '../../components/app/flask/experimental-area';
 ///: END:ONLY_INCLUDE_IF
@@ -49,7 +49,6 @@ import ReviewRecoveryPhrase from './recovery-phrase/review-recovery-phrase';
 import SecureYourWallet from './secure-your-wallet/secure-your-wallet';
 import ConfirmRecoveryPhrase from './recovery-phrase/confirm-recovery-phrase';
 import PrivacySettings from './privacy-settings/privacy-settings';
-// import CreationSuccessful from './creation-successful/creation-successful';
 import WalletReady from './wallet-ready/wallet-ready';
 import Welcome from './welcome/welcome';
 import ImportSRP from './import-srp/import-srp';
@@ -58,18 +57,18 @@ import MetaMetricsComponent from './metametrics/metametrics';
 import GetStarted from './get-started/get-started';
 import PasswordHint from './password-hint/password-hint';
 
-// const TWITTER_URL = 'https://twitter.com/MetaMask';
+const TWITTER_URL = 'https://twitter.com/MetaMask';
 
 export default function OnboardingFlow() {
   const [secretRecoveryPhrase, setSecretRecoveryPhrase] = useState('');
   const dispatch = useDispatch();
   const { pathname, search } = useLocation();
   const history = useHistory();
-  // const t = useI18nContext();
+  const t = useI18nContext();
   const completedOnboarding = useSelector(getCompletedOnboarding);
   const nextRoute = useSelector(getFirstTimeFlowTypeRouteAfterUnlock);
   const isFromReminder = new URLSearchParams(search).get('isFromReminder');
-  // const trackEvent = useContext(MetaMetricsContext);
+  const trackEvent = useContext(MetaMetricsContext);
   const isUnlocked = useSelector(getIsUnlocked);
 
   useEffect(() => {
@@ -225,7 +224,7 @@ export default function OnboardingFlow() {
           <Route exact path="*" component={OnboardingFlowSwitch} />
         </Switch>
       </div>
-      {/* {pathname === ONBOARDING_COMPLETION_ROUTE && (
+      {pathname === ONBOARDING_COMPLETION_ROUTE && (
         <Button
           className="onboarding-flow__twitter-button"
           type="link"
@@ -246,7 +245,7 @@ export default function OnboardingFlow() {
           <span>{t('followUsOnTwitter')}</span>
           <i className="fab fa-twitter onboarding-flow__twitter-button__icon" />
         </Button>
-      )} */}
+      )}
     </div>
   );
 }
