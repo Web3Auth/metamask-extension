81af7dce57f1845b0af8a61bbacd2e893361f57e
Lionell Briones <llenoil@gmail.com>
2025-04-24 15:33:35 +0800

    feat: srp input validation and fixes

--- ui/pages/onboarding-flow/import-srp/import-srp.js
+++ ui/pages/onboarding-flow/import-srp/import-srp.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
+import { isValidMnemonic } from '@ethersproject/hdnode';
 import {
   BlockSize,
   IconColor,
@@ -28,12 +29,17 @@ import {
   IconName,
   ButtonIcon,
   ButtonIconSize,
+  ButtonSize,
 } from '../../../components/component-library';
 import SRPDetailsModal from '../../../components/app/srp-details-modal';
 
+const hasUpperCase = (draftSrp) => {
+  return draftSrp !== draftSrp.toLowerCase();
+};
 export default function ImportSRP({ submitSecretRecoveryPhrase }) {
   const [secretRecoveryPhrase, setSecretRecoveryPhrase] = useState('');
   const [showSrpDetailsModal, setShowSrpDetailsModal] = useState(false);
+  const [srpError, setSrpError] = useState('');
   const history = useHistory();
   const hdEntropyIndex = useSelector(getHDEntropyIndex);
   const t = useI18nContext();
@@ -45,6 +51,36 @@ export default function ImportSRP({ submitSecretRecoveryPhrase }) {
     }
   }, [currentKeyring, history]);
   const trackEvent = useContext(MetaMetricsContext);
+
+  const onContinue = () => {
+    let newSrpError = '';
+    if (
+      hasUpperCase(secretRecoveryPhrase) ||
+      !isValidMnemonic(secretRecoveryPhrase)
+    ) {
+      newSrpError = t('invalidSeedPhraseNotFound');
+    }
+
+    setSrpError(newSrpError);
+
+    if (newSrpError) {
+      return;
+    }
+
+    submitSecretRecoveryPhrase(secretRecoveryPhrase);
+    trackEvent({
+      category: MetaMetricsEventCategory.Onboarding,
+      event: MetaMetricsEventName.OnboardingWalletSecurityPhraseConfirmed,
+      properties: {
+        hd_entropy_index: hdEntropyIndex,
+      },
+    });
+    history.replace(ONBOARDING_CREATE_PASSWORD_ROUTE);
+  };
+
+  useEffect(() => {
+    setSrpError('');
+  }, [secretRecoveryPhrase]);
 
   return (
     <div className="import-srp" data-testid="import-srp">
@@ -82,26 +118,27 @@ export default function ImportSRP({ submitSecretRecoveryPhrase }) {
       </div>
       <div className="import-srp__actions">
         <Box width={BlockSize.Full}>
-          <SrpInputImport onChange={setSecretRecoveryPhrase} />
+          <form onSubmit={(e) => e.preventDefault()}>
+            <SrpInputImport onChange={setSecretRecoveryPhrase} />
+            {srpError && (
+              <Box marginTop={2}>
+                <Text
+                  variant={TextVariant.bodySm}
+                  color={TextColor.errorDefault}
+                >
+                  {srpError}
+                </Text>
+              </Box>
+            )}
+          </form>
         </Box>
         <Button
           width={BlockSize.Full}
+          size={ButtonSize.Lg}
           className="import-srp__confirm-button"
           type="primary"
           data-testid="import-srp-confirm"
-          large
-          onClick={() => {
-            submitSecretRecoveryPhrase(secretRecoveryPhrase);
-            trackEvent({
-              category: MetaMetricsEventCategory.Onboarding,
-              event:
-                MetaMetricsEventName.OnboardingWalletSecurityPhraseConfirmed,
-              properties: {
-                hd_entropy_index: hdEntropyIndex,
-              },
-            });
-            history.replace(ONBOARDING_CREATE_PASSWORD_ROUTE);
-          }}
+          onClick={onContinue}
           disabled={!secretRecoveryPhrase.trim()}
         >
           {t('continue')}
