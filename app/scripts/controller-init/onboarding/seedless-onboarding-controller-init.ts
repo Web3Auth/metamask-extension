e8fb8c7f2a1584e1d2b86f6c6a1c42a8c2a3bc05
lwin <lwin@tor.us>
2025-05-06 10:24:29 +0800

    feat: update seedless onboarding controller init

--- app/scripts/controller-init/onboarding/seedless-onboarding-controller-init.ts
+++ app/scripts/controller-init/onboarding/seedless-onboarding-controller-init.ts
@@ -4,16 +4,35 @@ import {
   Web3AuthNetwork,
 } from '@metamask/seedless-onboarding-controller';
 import { ControllerInitFunction } from '../types';
+import { EncryptionKey, EncryptionResult } from '@metamask/browser-passworder';
+import { encryptorFactory } from '../../lib/encryptor-factory';
+
 
 export const SeedlessOnboardingControllerInit: ControllerInitFunction<
-  SeedlessOnboardingController,
+  SeedlessOnboardingController<EncryptionKey>,
   SeedlessOnboardingControllerMessenger
 > = (request) => {
   const { controllerMessenger, persistedState } = request;
+
+  const encryptor = encryptorFactory(600_000)
+
   const controller = new SeedlessOnboardingController({
     messenger: controllerMessenger,
     state: persistedState.SeedlessOnboardingController,
     network: Web3AuthNetwork.Devnet,
+    encryptor: {
+      ...encryptor,
+      decryptWithKey(key, encryptedData) {
+        let payload: EncryptionResult;
+        if (typeof encryptedData === 'string') {
+          payload = JSON.parse(encryptedData);
+        } else {
+          payload = encryptedData;
+        }
+
+        return encryptor.decryptWithKey(key, payload);
+      },
+    },
   });
 
   return {
