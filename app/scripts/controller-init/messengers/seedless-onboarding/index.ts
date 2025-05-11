721a1c0233ecaa1b54de2e6db366cc808fd50d24
lwin <lwin@tor.us>
2025-05-11 16:18:28 +0800

    feat: seedless-onboarding-controller init

---
+++ app/scripts/controller-init/messengers/seedless-onboarding/index.ts
@@ -0,0 +1,2 @@
+export { getSeedlessOnboardingControllerMessenger } from './seedless-onboarding-controller-messenger';
+export type { SeedlessOnboardingControllerInitMessenger as SeedlessOnboardingControllerMessenger } from './seedless-onboarding-controller-messenger';
