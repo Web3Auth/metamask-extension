29070f9ff3381f88c2d2756b2c5ef33a73210077
Tuna <tunguyenanh@tunatech.org>
2025-07-08 17:59:14 +0700

    feat: update socialSyncKeyringEncryptionKey function name

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -4701,7 +4701,7 @@ export default class MetamaskController extends EventEmitter {
         keyringId,
       );
 
-      await this.socialSyncKeyringEncryptionKey();
+      await this.syncKeyringEncryptionKey();
     } catch (error) {
       log.error('[createSeedPhraseBackup] error', error);
       throw error;
@@ -4795,7 +4795,7 @@ export default class MetamaskController extends EventEmitter {
         // update vault password to global password
         await this.keyringController.changePassword(password);
         // sync the new keyring encryption key after keyring chagnePassword to the seedless onboarding controller
-        await this.socialSyncKeyringEncryptionKey();
+        await this.syncKeyringEncryptionKey();
 
         // check password outdated again skip cache to reset the cache after successful syncing
         await this.seedlessOnboardingController.checkIsPasswordOutdated({
@@ -4811,12 +4811,12 @@ export default class MetamaskController extends EventEmitter {
     }
   }
 
-  async socialSyncKeyringEncryptionKey() {
-    const isSocialLoginFlow = this.onboardingController.getIsSocialLoginFlow();
-    if (!isSocialLoginFlow) {
-      return;
-    }
-
+  /**
+   * Syncs the keyring encryption key with the seedless onboarding controller.
+   *
+   * @returns {Promise<void>}
+   */
+  async syncKeyringEncryptionKey() {
     // store the keyring encryption key in the seedless onboarding controller
     const keyringEncryptionKey =
       await this.keyringController.exportEncryptionKey();
@@ -5177,7 +5177,7 @@ export default class MetamaskController extends EventEmitter {
             type: SecretType.Mnemonic,
           });
 
-          await this.socialSyncKeyringEncryptionKey();
+          await this.syncKeyringEncryptionKey();
         }
       }
     } finally {
